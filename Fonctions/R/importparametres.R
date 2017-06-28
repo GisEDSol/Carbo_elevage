9b2822a6a2a9c4b3fdde8fffdef93906366ffe7d
Rosalien <jeanbaptiste.paroissien@gmail.com>
2017-06-28 17:34:32 +0200

    renommage fichier

--- Fonctions/R/importparametres.R
+++ Fonctions/R/importparametres.R
@@ -30,7 +30,7 @@ ipak <- function(pkg){
 # Chargement des librairies
 listpaquets <- c("RODBC","gdata","fields","stringr","ggplot2","rgdal","maptools","RColorBrewer","classInt","devtools","reshape2","Hmisc","gridExtra","mapproj","wesanderson","FactoMineR",
 	"knitr","pander","GGally","factoextra","caret","plyr","doMC","sp","raster","RPostgreSQL","corrplot","MASS","foreign","doParallel","scales","viridis","mgcv","dplyr","car","fmsb"
-  ,"multcompView","outliers","xtable","debug") 
+  ,"multcompView","outliers","xtable","debug","AER","lmtest") 
 ipak(listpaquets)
 #new.packages <- listpaquets[!(listpaquets %in% installed.packages()[,"Package"])]
 #if(length(new.packages)) install.packages(new.packages)
